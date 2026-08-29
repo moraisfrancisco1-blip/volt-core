93e27da93c9d5e112116a07bf9adbd149b611dc6
moraisfrancisco1-blip <morais_francisco1@icloud.com>
2026-08-29 13:57:03 +0200

    Add dashboard entry point

---
+++ apps/web/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+function App() {
+  return <div>VOLT CORE</div>;
+}
+
+createRoot(document.getElementById('root')).render(<App />);
